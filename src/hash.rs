39c5a1b0c2233021204523114bf16e4fbfd3a58b
David Furey <david@furey.me.uk>
2022-07-11 22:38:38 +0100

    Switch to SHA-512 - apparently faster on 64 bit processors

--- src/hash.rs
+++ src/hash.rs
@@ -3,7 +3,7 @@
 use std::io;
 use std::fs;
 use std::path::Path;
-use sha2::{Sha256, Digest};
+use sha2::{Sha512, Digest};
 use hmac::{Hmac, Mac};
 // use sharding::ShardedChannel;
 use std::os::unix::ffi::OsStrExt;
@@ -11,7 +11,7 @@ use std::os::unix::ffi::OsStrExt;
 // use sqlite_cache::Cache;
 
 pub fn metadata(len: u64, mtime: i64, path: &Path) -> String {
-  let mut hasher = Sha256::new();
+  let mut hasher = Sha512::new();
   hasher.update(len.to_ne_bytes());
   hasher.update(mtime.to_ne_bytes());
   hasher.update(path.as_os_str().as_bytes());
@@ -19,8 +19,8 @@ pub fn metadata(len: u64, mtime: i64, path: &Path) -> String {
 }
 
 pub fn data(path: &Path, hmac_secret: &str) -> String {
-  type HmacSha256 = Hmac<Sha256>;
-  let mut hasher = HmacSha256::new_from_slice(hmac_secret.as_bytes())
+  type HmacSha512 = Hmac<Sha512>;
+  let mut hasher = HmacSha512::new_from_slice(hmac_secret.as_bytes())
       .expect("HMAC can take key of any size");
   let mut file = fs::File::open(path).unwrap();
   io::copy(&mut file, &mut hasher).unwrap();
