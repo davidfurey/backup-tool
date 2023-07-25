1475c5966db55a3ebeed59723325466c74360d4a
David Furey <david@furey.me.uk>
2023-07-25 21:20:34 +0100

    Use stream instead of FuturesOrdered

--- src/restore.rs
+++ src/restore.rs
@@ -8,7 +8,7 @@ use sequoia_openpgp::parse::Parse;
 use crate::datastore;
 use datastore::DataStore;
 extern crate rmp_serde as rmps;
-use crate::metadata_file;
+use crate::metadata_file::{self, FileMetadata};
 use metadata_file::FileData;
 use crate::decryption;
 use decryption::Decryption;
@@ -19,7 +19,7 @@ use filetype::FileType;
 use crate::swift::Bucket;
 use filetime::{set_file_mtime, FileTime};
 
-async fn download_file(data_hash: String, destination: PathBuf, bucket: &Bucket, data_prefix: String, cert: &Cert, cache: &PathBuf) {
+async fn download_file(data_hash: &str, destination: PathBuf, bucket: &Bucket, data_prefix: &str, cert: &Cert, cache: &PathBuf) {
   // todo: avoid repeating downloads
   let destination_filename = cache.join(format!("{}.gpg", data_hash));
   println!("attempting to create {:?}", destination_filename);
@@ -33,6 +33,57 @@ async fn download_file(data_hash: String, destination: PathBuf, bucket: &Bucket,
   let mut dest = File::create(&destination).unwrap();
   decryption::decrypt_file(&mut source, &mut dest, cert).unwrap();
   println!("decrypted {:?}", destination);
+}
+
+pub async fn process_file(entry: &FileMetadata, destination: PathBuf, data_bucket: &Bucket, data_prefix: &str, data_cache: &PathBuf, key: &Cert) -> i64 {
+  let canonical_name =  Path::new(entry.name.as_str());
+  let suffix = canonical_name.strip_prefix("/").unwrap();
+  let path = destination.join(suffix);
+  if !path.starts_with(&destination) { //canonicalize() ???
+    println!("ignoring file that is attempting to breach restore path"); // this might require more thought since we allow symlinks
+    //continue;
+    0
+  } else {
+    match entry.ttype {
+      FileType::FILE => {
+        println!("Creating file {:?}", &path);
+        let data_hash = match &entry.data_hash {
+            Some(val) => val.as_str(), 
+            None => "",
+        };
+        let downloaded = download_file(
+          data_hash, 
+          path.clone(), 
+          &data_bucket, 
+          data_prefix, 
+          &key, 
+          &data_cache
+        );
+        let mtime = FileTime::from_unix_time(entry.mtime, 0);
+        downloaded.map(move |f| {
+          set_file_mtime(&path, mtime).unwrap();
+          let permissions = PermissionsExt::from_mode(entry.mode);
+          set_permissions(&path, permissions).unwrap();
+          f
+        }).await;
+        1
+      }
+      FileType::SYMLINK => {
+        println!("Creating symlink {:?} -> {:?}", &path, entry.destination);
+        symlink(entry.destination.clone().unwrap(), &path).unwrap();
+        //let permissions = PermissionsExt::from_mode(entry.mode);
+        //set_permissions(&path, permissions).unwrap();
+        1
+      }
+      FileType::DIRECTORY => {
+        println!("Creating dir {:?}", &path);
+        create_dir_all(path.as_path()).unwrap();
+        let permissions = PermissionsExt::from_mode(entry.mode);
+        set_permissions(&path, permissions).unwrap();
+        1
+      }
+    }
+  }
 }
 
 pub async fn restore_backup(destination: PathBuf, backup: &String, store: &DataStore, key_file: PathBuf) {
@@ -52,41 +103,11 @@ pub async fn restore_backup(destination: PathBuf, backup: &String, store: &DataS
   let metadata: FileData = rmps::from_read(&mut decryptor).unwrap();
 
   println!("Destination: {:?}", destination.as_path());
-  let mut ft = FuturesOrdered::new();
-
-  for entry in metadata.data {
-    let canonical_name =  Path::new(entry.name.as_str());
-    let suffix = canonical_name.strip_prefix("/").unwrap();
-    let path = destination.join(suffix);
-    if !path.starts_with(&destination) { //canonicalize() ???
-      println!("ignoring file that is attempting to breach restore path"); // this might require more thought since we allow symlinks
-      continue;
-    }
-    match entry.ttype {
-      FileType::FILE => {
-        println!("Creating file {:?}", &path);
-        let downloaded = download_file(entry.data_hash.unwrap(), path.clone(), &data_bucket, store.data_prefix.clone(), &key, &data_cache);
-        let mtime = FileTime::from_unix_time(entry.mtime, 0);
-        ft.push(downloaded.map(move |f| {
-          set_file_mtime(&path, mtime).unwrap();
-          let permissions = PermissionsExt::from_mode(entry.mode);
-          set_permissions(&path, permissions).unwrap();
-          f
-        }));
-      }
-      FileType::SYMLINK => {
-        println!("Creating symlink {:?} -> {:?}", &path, entry.destination);
-        symlink(entry.destination.unwrap(), &path).unwrap();
-        //let permissions = PermissionsExt::from_mode(entry.mode);
-        //set_permissions(&path, permissions).unwrap();
-      }
-      FileType::DIRECTORY => {
-        println!("Creating dir {:?}", &path);
-        create_dir_all(path.as_path()).unwrap();
-        let permissions = PermissionsExt::from_mode(entry.mode);
-        set_permissions(&path, permissions).unwrap();
-      }
-    };
-  }
-  ft.collect().await
+  futures::stream::iter(metadata.data.iter())
+    .map(|entry| async {
+      process_file(entry, destination.clone(), &data_bucket, &store.data_prefix, &data_cache, &key).await
+    })
+    .buffer_unordered(4)
+    .count()
+    .await;
 }
